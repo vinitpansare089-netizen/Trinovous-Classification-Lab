e1d7ffa6d14dac83768ad03348097a8bccf2b3b6
vinitpansare089-netizen <vinitpansare089@gmail.com>
2026-04-11 22:25:40 +0530

    changes in main.py

--- student-dropout/main.py
+++ student-dropout/main.py
@@ -2,7 +2,15 @@ from src.data_loader import load_data
 
 df = load_data("data/raw/dataset.csv")
 
-print(df.head(10))
+# print(df.head(10))
+print(df.columns)
+# print(df.info())
+
+# print(df.nunique().sort_values(ascending=False))
+
+# print(df.corr(numeric_only=True)['Target'].sort_values())
+
+
 
 
 
