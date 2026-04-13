c90d2c46c652a5dd6186e8090a943c355f6dde16
vinitpansare089-netizen <vinitpansare089@gmail.com>
2026-04-13 18:12:50 +0530

    functions

--- student-dropout/src/preprocessing.py
+++ student-dropout/src/preprocessing.py
@@ -1,7 +1,34 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
+import joblib
+from sklearn.preprocessing import StandardScaler
 
-def preprocess_data(df):
-
-    #convert categorical into binary/boolean
+def encode_data(df):
     df = pd.get_dummies(df, drop_first=True)
+    return df
+
+def feature_engineering(df):
+    return 
+
+def split_features(df):
+    X = df.drop("target", axis = 1)
+    y = df["target"]
+
+    return X, y
+
+def split_data(X, y):
+    return train_test_split(X, y, test_size=0.2, random_state=42)
+
+def scale_data(X_train, X_test):
+
+    scaler = StandardScaler()
+
+    X_train = scaler.fit_transform(X_train)
+    X_test = scaler.transform(X_test)
+
+    joblib.dump(scaler, "models/scaler.pkl")
+
+    return X_train, X_test
+
+
 
