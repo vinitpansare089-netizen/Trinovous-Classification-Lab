04abfcfb81b41a9b30c59d4bbcc9a2d48b9e6521
vinitpansare089-netizen <vinitpansare089@gmail.com>
2026-04-13 18:55:10 +0530

    modular function build

---
+++ student-dropout/src/evaluate.py
@@ -0,0 +1,9 @@
+from sklearn.metrics import classification_report, accuracy_score,f1_score,confusion_matrix
+
+def evaluate_model(model, x_test, y_test):
+    y_pred = model.predict(x_test)
+
+    print("Accuracy:", accuracy_score(y_test, y_pred))
+    print("\nClassification Report:\n", classification_report(y_test, y_pred))
+    print("\nconfusion matrix:\n", confusion_matrix(y_test, y_pred))
+
