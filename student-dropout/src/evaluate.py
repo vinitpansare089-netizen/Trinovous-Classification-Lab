a89826757587a79f45b5c59f86e0633b9f7aa50e
vinitpansare089-netizen <vinitpansare089@gmail.com>
2026-04-30 19:29:48 +0530

    commit changes

--- student-dropout/src/evaluate.py
+++ student-dropout/src/evaluate.py
@@ -7,4 +7,3 @@ def evaluate_model(model, x_test, y_test):
     print("\nClassification Report:\n", classification_report(y_test, y_pred))
     print("\nconfusion matrix:\n", confusion_matrix(y_test, y_pred))
     print("\ f1 score:\n", f1_score(y_test, y_pred))
-
